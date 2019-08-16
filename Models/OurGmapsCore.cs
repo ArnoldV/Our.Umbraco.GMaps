c19977f01b07b213a7b797643c408231ced3eb17
Arnold Visser <arnold@axendo.nl>
2019-08-16 15:43:06 +0200

    Code for Property Value Converter & Model

---
+++ Models/OurGmapsCore.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axendo.Umb.Watersportverbond.Platform.Web.Core.Models.GMaps
+{
+    public class OurGmapsCore
+    {
+        [JsonProperty("latlon")]
+        public string Coordinates { get; set; }
+        [JsonProperty("full_address")]
+        public string FullAddress { get; set; }
+        [JsonProperty("postcode")]
+        public string PostCode { get; set; }
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+    }
+}
---
+++ ProprtyValueConverter/GMapsPropertyValueConverter.cs
@@ -0,0 +1,46 @@
+﻿using Axendo.Umb.Watersportverbond.Platform.Web.Core.Models.GMaps;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Axendo.Umb.Watersportverbond.Platform.Web.Core.Extensions
+{
+    public class GMapsPropertyValueConverter : PropertyValueConverterBase
+    {
+        public override Type GetPropertyValueType(IPublishedPropertyType propertyType)
+        {
+            return typeof(OurGmapsCore);
+        }
+
+        public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType)
+        {
+            return PropertyCacheLevel.Element;
+        }
+
+
+        public override bool IsConverter(IPublishedPropertyType propertyType)
+        {
+            return propertyType.EditorAlias.Equals("Our.Gmaps.Core");
+        }
+
+        public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+        public override object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+        {
+            if (inter != null)
+            {
+                return JsonConvert.DeserializeObject<OurGmapsCore>(inter.ToString());
+            }
+
+            return null;
+        }
+    }
+}
