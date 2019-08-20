5f62d2260bb43a65426605c68c04ba786eadcce2
Arnold Visser <arnold@axendo.nl>
2019-08-20 16:55:45 +0200

    postcode ==> postalcode

--- src/Our.Umbraco.GMaps.Core/Models/GmapsAddress.cs
+++ src/Our.Umbraco.GMaps.Core/Models/GmapsAddress.cs
@@ -8,8 +8,8 @@ namespace Our.Umbraco.GMaps.Models
         public string Coordinates { get; set; }
         [JsonProperty("full_address")]
         public string FullAddress { get; set; }
-        [JsonProperty("postcode")]
-        public string PostCode { get; set; }
+        [JsonProperty("postalcode")]
+        public string PostalCode { get; set; }
         [JsonProperty("city")]
         public string City { get; set; }
         [JsonProperty("state")]
