dea18453f96fe1232b56c155d216b7230c55fd8c
Arnold Visser <arnold@axendo.nl>
2019-08-21 12:06:27 +0200

    [WIP] SnazzyMaps integration - Model still to do

--- src/Our.Umbraco.GMaps.Core/App_Plugins/Our.Umbraco.GMaps/js/our.umbraco.gmaps.snazzymaps.controller.js
+++ src/Our.Umbraco.GMaps.Core/App_Plugins/Our.Umbraco.GMaps/js/our.umbraco.gmaps.snazzymaps.controller.js
@@ -22,58 +22,23 @@
             };
 
             if ($scope.model.value) {
+                console.log($scope.model.value);
                 if ($scope.model.value.selectedstyle) {
                     $scope.selectedStyle = $scope.model.value.selectedstyle;
                 }
                 if ($scope.model.value.apiKey) {
                     $scope.apiKey = $scope.model.value.apiKey;
                 }
-
-                console.log($scope.model.value);
             }
 
             $scope.getApi = function (endpoint) {
                 $scope.endpoint = endpoint;
-                if ($scope.apiKey) {
-                    switch (endpoint) {
-                        case 'explore':
-                            OurGmapsSnazzyMapsFactory.Explore($scope.apiKey, $scope.options.pageNumber).then(
-                                function (response) {
-                                    $scope.pagination = response.pagination;
-                                    $scope.styles = response.styles;
-                                    $scope.model.value.apiKey = $scope.apiKey;
-                                });
-                            break;
-                        case 'Favorites':
-                            OurGmapsSnazzyMapsFactory.Favorites($scope.apiKey, $scope.options.pageNumber).then(
-                                function (response) {
-                                    $scope.pagination = response.pagination;
-                                    $scope.styles = response.styles;
-                                    $scope.model.value.apiKey = $scope.apiKey;
-                                });
-                            break;
-                        case 'my-styles':
-                            OurGmapsSnazzyMapsFactory.MyStyles($scope.apiKey, $scope.options.pageNumber).then(
-                                function (response) {
-                                    $scope.pagination = response.pagination;
-                                    $scope.styles = response.styles;
-                                    $scope.model.value.apiKey = $scope.apiKey;
-                                });
-                            break;
-                        default:
-                            OurGmapsSnazzyMapsFactory.Explore(vm.apiKey, $scope.options.pageNumber).then(
-                                function(response) {
-                                    $scope.pagination = response.pagination;
-                                    $scope.styles = response.styles;
-                                    $scope.model.value.apiKey = $scope.apiKey;
-                                });
 
-                            $scope.model.value.apiKey = vm.apiKey;
-                    }
-                } else {
-                    $scope.error = "No (valid) SnazzyMaps API Key";
-                }
-
+                OurGmapsSnazzyMapsFactory.GetMapStyles($scope.apiKey, endpoint, $scope.options.pageNumber).then(
+                    function (response) {
+                        $scope.pagination = response.pagination;
+                        $scope.styles = response.styles;
+                    });
             };
 
             $scope.goToPage = function (pageNumber) {
@@ -88,5 +53,15 @@
                 // set value 
                 $scope.model.value = $scope.saveData;
             };
+
+            $scope.removeSelectedStyle = function () {
+                $scope.selectedStyle = {};
+                $scope.saveData.selectedstyle = $scope.selectedStyle;
+                $scope.saveData.apiKey = $scope.apiKey;
+                // set value 
+                $scope.model.value = $scope.saveData;
+            };
         }
+
+
     ]);
--- src/Our.Umbraco.GMaps.Core/App_Plugins/Our.Umbraco.GMaps/js/our.umbraco.gmaps.snazzymaps.factory.js
+++ src/Our.Umbraco.GMaps.Core/App_Plugins/Our.Umbraco.GMaps/js/our.umbraco.gmaps.snazzymaps.factory.js
@@ -3,52 +3,29 @@
     function ($http, $q) {
 
         return {
-            Explore: function (apiKey, pageNumber) {
+            GetMapStyles: function (apiKey, method, pageNumber) {
                 var deferred = $q.defer();
+                if (apiKey) {
+                    if (!method) {
+                        method = 'explore';
+                    }
 
-                $http({
-                    method: 'get',
-                    url: 'https://snazzymaps.com/explore.json?key=' + apiKey + '&page=' + pageNumber
-                }).then(function (response) {
-                    deferred.resolve({
-                        pagination: response.data.pagination,
-                        styles: response.data.styles
-                    });
-                }, function (error) {
-                    console.log(error, 'can not get data.');
-                });
-                return deferred.promise;
-            },
-            Favorites: function (apiKey, pageNumber) {
-                var deferred = $q.defer();
-
-                $http({
-                    method: 'get',
-                    url: 'https://snazzymaps.com/favorites.json?key=' + apiKey + '&page=' + pageNumber
-                }).then(function (response) {
-                    deferred.resolve({
-                        pagination: response.data.pagination,
-                        styles: response.data.styles
-                    });
-                }, function (error) {
-                    console.log(error, 'can not get data.');
-                });
-                return deferred.promise;
-            },
-            MyStyles: function (apiKey, pageNumber) {
-                var deferred = $q.defer();
-                $http({
-                    method: 'get',
-                    url: 'https://snazzymaps.com/my-styles.json?key=' + apiKey + '&page=' + pageNumber
-                }).then(function (response) {
-                    deferred.resolve({
-                        pagination: response.data.pagination,
-                        styles: response.data.styles
-                    });
-                }, function (error) {
-                    console.log(error, 'can not get data.');
-                });
-                return deferred.promise;
+                    $http({
+                        method: 'get',
+                        url: 'https://snazzymaps.com/' + method + '.json?key=' + apiKey + '&page=' + pageNumber
+                    }).then(function (response) {
+                        deferred.resolve({
+                            pagination: response.data.pagination,
+                            styles: response.data.styles
+                        });
+                    },
+                        function (error) {
+                            console.log(error, 'can not get data ');
+                        });
+                    return deferred.promise;
+                } else {
+                    console.log("No (valid) SnazzyMaps API Key");
+                }
             }
         };
     }
