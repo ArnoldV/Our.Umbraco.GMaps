182eabc5bcd7ea7f4dd468084cbe700fb3b0a002
Robert Foster <robert@youritgroup.com.au>
2021-11-25 12:36:58 +1100

    cleaned up usings

--- Our.Umbraco.GMaps.Core/Controllers/GoogleMapsController.cs
+++ Our.Umbraco.GMaps.Core/Controllers/GoogleMapsController.cs
@@ -1,56 +1,19 @@
 ﻿using Our.Umbraco.GMaps.Core.Utility;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 #if NET5_0_OR_GREATER
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Umbraco.Cms.Core;
-using Umbraco.Cms.Core.Dictionary;
-using Umbraco.Cms.Core.Events;
-using Umbraco.Cms.Core.Mapping;
-using Umbraco.Cms.Core.Models;
-using Umbraco.Cms.Core.Models.ContentEditing;
-using Umbraco.Cms.Core.PropertyEditors;
-using Umbraco.Cms.Core.Scoping;
-using Umbraco.Cms.Core.Security;
-using Umbraco.Cms.Core.Serialization;
-using Umbraco.Cms.Core.Services;
-using Umbraco.Cms.Core.Strings;
 using Umbraco.Cms.Web.Common.Attributes;
-using Umbraco.Cms.Web.Common.Authorization;
-using Umbraco.Cms.Web.Common.Security;
 using Umbraco.Cms.Web.Common.Controllers;
-using Umbraco.Extensions;
 #else
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Web.Http;
-using System.Web.Security;
-using System.Web;
-using System.Web.Hosting;
 using Umbraco.Web.WebApi;
 using Umbraco.Core.Cache;
 using Umbraco.Core.Configuration;
 using Umbraco.Core.Logging;
-using Umbraco.Core.Models;
 using Umbraco.Core.Persistence;
-using Umbraco.Core.Persistence.DatabaseModelDefinitions;
-using Umbraco.Core.PropertyEditors;
 using Umbraco.Core.Services;
-using Umbraco.Core.Security;
 using Umbraco.Web;
-using Umbraco.Web.Editors;
-using Umbraco.Web.Models.ContentEditing;
 using Umbraco.Web.Mvc;
-using Umbraco.Web.WebApi.Filters;
-using static Umbraco.Core.Constants;
 #endif
 
 namespace Our.Umbraco.GMaps.Core.Controllers
