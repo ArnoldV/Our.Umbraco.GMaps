e4e7f8a9c3b0de1706106335c7dd15d02cce5d83
Robert Foster <robert@youritgroup.com.au>
2021-12-14 19:26:13 +1100

    Improved Settings management and fixed issue retrieving settings for plugin

--- Our.Umbraco.GMaps.Core/Models/Address.cs
+++ Our.Umbraco.GMaps.Core/Models/Address.cs
@@ -1,35 +1,45 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Runtime.Serialization;
 
 namespace Our.Umbraco.GMaps.Models
 {
     public class Address
     {
+        [DataMember(Name = "coordinates")]
         [JsonProperty("coordinates")]
         public Location Coordinates { get; set; }
 
+        [DataMember(Name = "latlng")]
         [JsonProperty("latlng")]
         [Obsolete("Used only for backwards compatibility with Our.Umbraco.GMaps for Umbraco 8")]
         public string LatLng { get; set; }
 
+        [DataMember(Name = "lat")]
         [JsonProperty("lat")]
         public string Latitude { get; set; }
 
+        [DataMember(Name = "lng")]
         [JsonProperty("lng")]
         public string Longitude { get; set; }
 
+        [DataMember(Name = "full_address")]
         [JsonProperty("full_address")]
         public string FullAddress { get; set; }
 
+        [DataMember(Name = "postalcode")]
         [JsonProperty("postalcode")]
         public string PostalCode { get; set; }
 
+        [DataMember(Name = "city")]
         [JsonProperty("city")]
         public string City { get; set; }
 
+        [DataMember(Name = "state")]
         [JsonProperty("state")]
         public string State { get; set; }
 
+        [DataMember(Name = "country")]
         [JsonProperty("country")]
         public string Country { get; set; }
 
