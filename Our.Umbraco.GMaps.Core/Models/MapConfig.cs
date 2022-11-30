67bea2df3c0908d062d5de6de479d950319a58cf
Robert Foster <robert@youritgroup.com.au>
2022-11-30 19:34:32 +1100

    Fix for #126

--- Our.Umbraco.GMaps.Core/Models/MapConfig.cs
+++ Our.Umbraco.GMaps.Core/Models/MapConfig.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System.Runtime.Serialization;
+#if NET5_0_OR_GREATER
+using System.Text.Json.Serialization;
+#endif
 
 namespace Our.Umbraco.GMaps.Models
 {
@@ -8,23 +11,38 @@ namespace Our.Umbraco.GMaps.Models
     {
         [DataMember(Name = "apikey")]
         [JsonProperty("apikey")]
+#if NET5_0_OR_GREATER
+        [JsonPropertyName("apikey")]
+#endif
         public string ApiKey { get; set; }
 
         [DataMember(Name = "zoom")]
         [JsonProperty("zoom")]
+#if NET5_0_OR_GREATER
+        [JsonPropertyName("zoom")]
+#endif
         public int Zoom { get; set; }
 
         [DataMember(Name = "centerCoordinates")]
         [JsonProperty("centerCoordinates")]
+#if NET5_0_OR_GREATER
+        [JsonPropertyName("centerCoordinates")]
+#endif
         public Location CenterCoordinates { get; set; } = new Location();
 
         [DataMember(Name = "mapstyle")]
         [JsonProperty("mapstyle")]
+#if NET5_0_OR_GREATER
+        [JsonPropertyName("mapstyle")]
+#endif
         public string Style { get; set; }
 
         [DataMember(Name = "maptype")]
         [JsonProperty("maptype")]
-        [JsonConverter(typeof(StringEnumConverter))]
+#if NET5_0_OR_GREATER
+        [JsonPropertyName("maptype")]
+#endif
+        [System.Text.Json.Serialization.JsonConverter(typeof(StringEnumConverter))]
         public MapType? MapType { get; set; }
     }
 }
