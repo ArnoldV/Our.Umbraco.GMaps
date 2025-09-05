e22dabb31a79300e13b793866f2292e026cd7a3e
Robert Foster <robert@youritgroup.com.au>
2025-09-05 14:47:31 +1000

    WIP fix up client directory casing pass 1

--- Our.Umbraco.GMaps/client/src/entrypoints/manifest.ts
+++
@@ -1,8 +0,0 @@
-export const manifests: Array<UmbExtensionManifest> = [
-  {
-    name: "GMaps Entrypoint",
-    alias: "GMaps.Entrypoint",
-    type: "backofficeEntryPoint",
-    js: () => import("./entrypoint"),
-  }
-];
--- Our.Umbraco.GMaps/client/src/repository/manifest.ts
+++
@@ -1,8 +0,0 @@
-export const manifests: Array<UmbExtensionManifest> = [
-  {
-    type: 'repository',
-    alias: 'GMaps.Repository.Settings',
-    name: 'GMaps Settings Repository',
-    api: () => import('./settings.repository.js'),
-  }
-];
